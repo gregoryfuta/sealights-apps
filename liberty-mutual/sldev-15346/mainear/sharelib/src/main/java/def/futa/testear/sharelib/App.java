cf7f60a77e66cc40616c86945f366a69323b461d
Gregory Futa <gregory.futa@sealights.io>
2023-05-26 15:27:43 +0300

    [SLDEV-15346] EAR tests

---
+++ liberty-mutual/sldev-15346/mainear/sharelib/src/main/java/def/futa/testear/sharelib/App.java
@@ -0,0 +1,13 @@
+package def.futa.testear.sharelib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
